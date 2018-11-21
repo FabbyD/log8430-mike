06c65a50205b4169a09f883daab2019e511868c2
Fabrice Dugas <fabby.dugas@gmail.com>
2018-11-21 12:46:49 -0500

    Basic spark integration

--- tp4/server.py
+++ tp4/server.py
@@ -1,6 +1,26 @@
+import sys
+
 from flask import Flask
+from operator import add
+from pyspark.sql import SparkSession
+
+spark = SparkSession\
+    .builder\
+    .appName("PythonWordCount")\
+    .getOrCreate()
+
 app = Flask(__name__)
+
 
 @app.route('/', methods=['GET'])
 def hello_world():
-    return 'Hello, World!'
+    filename = "foo.txt"
+    lines = spark.read.text(filename).rdd.map(lambda r: r[0])
+    counts = lines.flatMap(lambda x: x.split(' ')) \
+                  .map(lambda x: (x, 1)) \
+                  .reduceByKey(add)
+    output = counts.collect()
+    result = 0
+    for (word, count) in output:
+        result += count
+    return "There are %d words" % result
