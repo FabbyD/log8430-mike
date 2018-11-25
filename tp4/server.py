ad3ea85af160bb4c503997267fe87741493ff0f5
Fabrice Dugas <fabby.dugas@gmail.com>
2018-11-25 13:15:54 -0500

    Add POST method

--- tp4/server.py
+++ tp4/server.py
@@ -17,11 +17,31 @@ spark = SparkSession \
 df = spark.read.format("com.mongodb.spark.sql.DefaultSource").load()
 
 # Start REST server
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 app = Flask(__name__)
 
+class Facture:
+    def __init__(self, data):
+        self.produits = data.get('produits', [])
+        self.prix = [ float(p) for p in data.get('prix', []) ]
+
+    def values(self):
+        return self.__dict__.values()
+    
+    def keys(self):
+        return self.__dict__.keys()
+
 @app.route('/', methods=['GET'])
-def hello_world():
+def chercher_produits():
     rows = df.select("*")
     products = rows.rdd.map(lambda x: x.produits).first()
     return jsonify(products)
+
+@app.route('/', methods=['POST'])
+def sauvegarder_facture():
+    data = request.json
+    facture = Facture(data)
+    df = spark.createDataFrame([tuple(facture.values())], tuple(facture.keys()))
+    df.write.format("com.mongodb.spark.sql.DefaultSource").mode("append").save()
+    df.show()
+    return jsonify({"success": True})
