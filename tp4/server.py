252103c58fb3576eb15056d716f4cc76ee62ae86
Fabrice Dugas <fabrice.dugas@polymtl.ca>
2018-11-30 18:39:04 -0800

    Configuration du serveur avec les bonnes adresses ip

--- tp4/server.py
+++ tp4/server.py
@@ -3,10 +3,23 @@ from pyspark import SparkConf
 from pyspark.sql import SparkSession
 from pyspark.mllib.fpm import FPGrowth
 
+import sys
+
+with open("config", "r") as cfile:
+    ips = [ip.strip() for ip in cfile]
+
+if len(ips) < 2:
+    print("Veuillez specifier les IP de spark et Mongo dans le fichier config")
+    sys.exit()
+
+sparkmasterip = ips[0]
+mongoip = ips[1]
+
 conf = SparkConf() \
         .setAppName("TP4") \
-        .set("spark.mongodb.input.uri", "mongodb://127.0.0.1/tp4.factures") \
-        .set("spark.mongodb.output.uri", "mongodb://127.0.0.1/tp4.factures") \
+        .setMaster("spark://%s:7077" % sparkmasterip) \
+        .set("spark.mongodb.input.uri", "mongodb://%s:27017/tp4.factures" % mongoip) \
+        .set("spark.mongodb.output.uri", "mongodb://%s:27017/tp4.factures" % mongoip) \
         .set("spark.jars.packages", "org.mongodb.spark:mongo-spark-connector_2.11:2.3.1")
 
 spark = SparkSession \
@@ -36,7 +49,7 @@ class Facture:
 def chercher_produits():
     products = df.rdd.map(lambda x : x.produits)
     model = FPGrowth.train(products, minSupport=0.4, numPartitions=5)
-    result = model.freqItemsets().flatMap(lambda itemset : itemset.items).collect()
+    result = list(set(model.freqItemsets().flatMap(lambda itemset : itemset.items).collect()))
     print("Frequent items: %s" % str(result))
     return jsonify(result)
 
